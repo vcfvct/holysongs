63e6acfc3b6f15cf055ab2771e86adf9eddd7199
vcfvct@gmail.com <vcfvct@gmail.com@4229813d-681b-4579-70fc-31b89afcfafe>
2013-10-06 21:30:44 +0000

    init

---
+++ trunk/src/com/example/HolySongs/DisplayLyricActivity.java
@@ -0,0 +1,31 @@
+package com.example.HolySongs;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LeOn
+ * Date: 13-10-5
+ * Time: 下午10:13
+ */
+public class DisplayLyricActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.LYRIC);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setTextSize(20);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
+}
