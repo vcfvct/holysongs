395a1292692e412268ee259b9687863ddb948c28
vcfvct@gmail.com <vcfvct@gmail.com@4229813d-681b-4579-70fc-31b89afcfafe>
2013-11-04 02:23:43 +0000

    1. upgrade to version number 2.0 2. add youtube, tudou, youku search for video 3. add 3g usage warning if wifi is not connected. 4. add menu in the lyric view as video entrypoint

--- src/com/goodtrendltd/HolySongs/SettingsActivity.java
+++ src/com/goodtrendltd/HolySongs/SettingsActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
